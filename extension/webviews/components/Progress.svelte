6a8fa456357397378c49c03f194007640bd5160d
Paul Kremer <paul.kremer@fau.de>
2022-11-02 19:39:16 +0100

    prgress changes

--- extension/webviews/components/Progress.svelte
+++ extension/webviews/components/Progress.svelte
@@ -1,39 +1,48 @@
-<script>
+<script lang="ts">
+    import { onMount } from "svelte";
     let levelIcon = "🔴";
     let levelNumber = 1;
     let levelName = "Beginner";
     let currentPoints = 0;
     let maxLevelPoints = 100;
+
+    onMount(() => {
+        window.addEventListener("message", (event) => {
+            const message = event.data;
+            switch (message.type) {
+                case "copilot-change":
+                    currentPoints += message.value.length;
+            }
+        });
+    });
 </script>
 
-<style>
-
-.statusWrapper {
-    text-align: center;
-    border: 1px solid black;
-    overflow: hidden; /* will contain if #first is longer than #second */
-}
-.levelStatus {
-    float:left; /* add this */
-    border: 1px solid red;
-}
-.pointsStatus {
-    border: 1px solid green;
-    overflow: hidden; /* if you don't want #second to wrap below #first */
-}
-</style>
-
-
 <h3>Progress</h3>
-<div class=statusWrapper></div>
+<div class="statusWrapper" />
 <div class="levelStatus">
-    Level<br/>
-    {levelIcon}<br/>
-    {levelNumber}<br/>
-    {levelName}<br/>
+    Level<br />
+    {levelIcon}<br />
+    {levelNumber}<br />
+    {levelName}<br />
 </div>
 <div class="pointsStatus">
-    Pilotpoints<br/>
-    <progress value="30" max="100"></progress><br/>
-    {currentPoints}/{maxLevelPoints}<br/>
+    Pilotpoints<br />
+    <progress value="30" max="100" /><br />
+    {currentPoints}/{maxLevelPoints}<br />
 </div>
+
+<style>
+    .statusWrapper {
+        text-align: center;
+        border: 1px solid black;
+        overflow: hidden; /* will contain if #first is longer than #second */
+    }
+    .levelStatus {
+        float: left; /* add this */
+        border: 1px solid red;
+    }
+    .pointsStatus {
+        border: 1px solid green;
+        overflow: hidden; /* if you don't want #second to wrap below #first */
+    }
+</style>
--- extension/webviews/components/Sidebar.svelte
+++ extension/webviews/components/Sidebar.svelte
@@ -12,7 +12,7 @@
                     lastChange=message.value;
             }
         });
-    });
+    }); 
 </script>
 
 <Progress/>
